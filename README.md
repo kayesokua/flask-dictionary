8bc62c16713e0ecbc184c3c40084330988d91229
kayesokua <kaye.sokua@code.berlin>
2023-01-29 19:19:48 +0100

    grouping related functions for readable code

--- README.md
+++ README.md
@@ -6,14 +6,14 @@
 View Demo: https://dictionary-data-api.onrender.com
 
 QuadLingo is a user-friendly tool for searching slangs and their meanings in English, Deutsch, Mandarin and Tagalog.
-This app serves as a basic application of the Flask Framework using Google Sheets as its source of information and easy deployment on Render. For a setup that requires a relational database setup, [view this project instead](https://github.com/kayesokua/flask-store-dummy).
+This app serves as a basic application of the Flask Framework using Google Sheets as its source of information and easy deployment on Render. Google Sheets as a database offers non-developers a simple, accessible, and cost-effective solution for storing and managing non-sensitive data. For a Flask setup that will process sensitive data, [view this project instead](https://github.com/kayesokua/flask-store-dummy).
 
 ## Package Requirements
-1. `Flask`
-2. `gTTS`
-3. `gunicorn`
-4. `jsons`
-5. `urllib3`
+1. `Flask` provides a simple and flexible way to build web applications. Without Flask, you would have to write low-level code to handle HTTP requests, handle routing, and handle templates, among other things.
+2. `gTTS` generates text-to-speech using Google Translate
+3. `gunicorn` is a production-ready HTTP server for Python web applications. Flask, by itself, is not designed to be used in production and is only meant to be used for development purposes.
+4. `jsons`  to parse the data received from the Google Sheets API
+5. `urllib3`makes an HTTP request to the Google Sheets API
 
 ## Run the Application on Local Environment
 1. Clone repository `git clone https://github.com/kayesokua/flask-dictionary flask-dir`
@@ -24,12 +24,22 @@ This app serves as a basic application of the Flask Framework using Google Sheet
 ## How to Use
 
 ### Get API JSON
-
 *  `https://dictionary-data-api.onrender.com/api/json/` to to get all items in the dictionary
 *  `https://dictionary-data-api.onrender.com/api/json/<int>` to get 1 item detail
 
-### Create Your Own Dictionary
+### Modify Dictionary
+Currently the app does not have a single page to configure the contents of the dictionary, but here is the list of instructions on how you can modify the content.
 
+1. Create a new spreadsheet using Google Sheets. The columns represent the key names and the rows will be the dictionary's items. [Follow this format](https://docs.google.com/spreadsheets/d/1b-ri2DlWLIhqB-E8xUCqnh9fsxJJ5J-KGV52rJ250Dk/edit#gid=81166623)
+
+2. Publish the spreadsheet to the web and get the generated end point `https://spreadsheets.google.com/feeds/cells/<your_google_sheet_code>/<sheet_page_no>/public/full?alt=json`
+
+3. Go to `app.py` and change the url source.
+```python
+self.dictionary_url = "<your google sheet json url>"
+```
+
+4. 
 
 ## Resources 
 1. [Flask Documentation](https://flask.palletsprojects.com/en/2.2.x/)
--- app.py
+++ app.py
@@ -7,46 +7,70 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+class DictionaryAPI:
+    def __init__(self):
+        self.dictionary_url = "https://sheets.googleapis.com/v4/spreadsheets/1b-ri2DlWLIhqB-E8xUCqnh9fsxJJ5J-KGV52rJ250Dk/values/main?alt=json&key=AIzaSyB1Wz6Hm3vj4KYuRdlZ8m4N5YB_GJnjF8k"
+        with urllib.request.urlopen(self.dictionary_url) as url:
+            self.dictionary_data = json.load(url)
+            self.dictionary_data = self.dictionary_data["values"]
+            self.dictionary_data.pop(0)
+            self.dictionary_datac = list(map(lambda x: list(map(lambda y: y.casefold(), x)), self.dictionary_data))
+        self.dictionary_len = len(self.dictionary_datac) - 1
 
-# Requesting data from google sheets and reformatting
-# Global Functions
-dictionary_url = "https://sheets.googleapis.com/v4/spreadsheets/1b-ri2DlWLIhqB-E8xUCqnh9fsxJJ5J-KGV52rJ250Dk/values/main?alt=json&key=AIzaSyB1Wz6Hm3vj4KYuRdlZ8m4N5YB_GJnjF8k"
-with urllib.request.urlopen(dictionary_url) as url:
-    dictionary_data = json.load(url)
-    dictionary_data = dictionary_data["values"]
-    dictionary_data.pop(0)
+    def search(self, keyword):
+        keyword = keyword.casefold()
+        if keyword == '' or len(keyword) < 4:
+            return None
+        else:
+            results = [sublist for sublist in self.dictionary_datac for item in sublist if keyword in item]
+            return results
 
-    # To avoid case-sensitve searches
-    dictionary_datac = list(map(lambda x: list(map(lambda y: y.casefold(), x)), dictionary_data))
-dictionary_len = len(dictionary_datac)-1
+    def get_random(self):
+        i = random.randint(0, self.dictionary_len - 1)
+        random_item = {
+            "type": self.dictionary_data[i][0],
+            "en": self.dictionary_data[i][1],
+            "de": self.dictionary_data[i][2],
+            "zh-cn": self.dictionary_data[i][3],
+            "pinyin": self.dictionary_data[i][4],
+            "ph-tl": self.dictionary_data[i][5]
+        }
+        return random_item
 
-# Route for home page that starts with a random word
+    def get_json_all(self):
+        return jsonify(self.dictionary_data)
+
+    def get_json_one(self, param):
+        i = int(param)
+        try:
+            result = {
+                "type": self.dictionary_data[i][0],
+                "en": self.dictionary_data[i][1],
+                "de": self.dictionary_data[i][2],
+                "zh-cn": self.dictionary_data[i][3],
+                "pinyin": self.dictionary_data[i][4],
+                "ph-tl": self.dictionary_data[i][5],
+            }
+            return jsonify(result)
+        except IndexError:
+            message = "At the moment, the dictionary only have {} items.".format(self.dictionary_len)
+            return render_template("error.html", error=message)
+
+dictionary_api = DictionaryAPI()
+
+# Home page and search functionality:
 @app.route("/")
 def index():
-    i = random.randint(0,dictionary_len)
-    results = []
-    random_item = {
-        "type": dictionary_data[i][0],
-        "en": dictionary_data[i][1],
-        "de": dictionary_data[i][2],
-        "zh-cn": dictionary_data[i][3],
-        "pinyin": dictionary_data[i][4],
-        "ph-tl": dictionary_data[i][5]
-        }
+    random_item = dictionary_api.get_random()
     return render_template("home.html", random=random_item, masonry = False)
 
-# Route for searching a keyword using Get Method
-@app.route("/search", methods=["GET"])
+@app.route("/search")
 def search_item():
     keyword = request.args.get('keyword')
-    keyword = keyword.casefold()
-    if keyword == '' or len(keyword) < 4:
-        return redirect(url_for('index'))
-    else:
-        results = [sublist for sublist in dictionary_datac for item in sublist if keyword in item]
-        return render_template("home.html",results=results,keyword=keyword, masonry=True)
+    results = dictionary_api.search(keyword)
+    return render_template("home.html",results=results,keyword=keyword, masonry=True)
 
-# Generating speech URL for the audio source
+# Speech generation
 @app.route("/speech/<lang>/<text>")
 def generate_speech_url(lang,text):
     tts = gtts.gTTS(text=text, lang=lang)
@@ -55,28 +79,16 @@ def generate_speech_url(lang,text):
     fp.seek(0)
     return Response(fp.read(), content_type="audio/mpeg")
 
-# API Routes
+#API endpoints
 @app.route("/api/json/")
 def get_api_json_all():
-    return jsonify(dictionary_data)
+    return dictionary_api.get_json_all()
 
 @app.route("/api/json/<int:param>")
 def get_api_json_one(param):
-    i = int(param)
-    try:
-        result = {
-            "type": dictionary_data[i][0],
-            "en": dictionary_data[i][1],
-            "de": dictionary_data[i][2],
-            "zh-cn": dictionary_data[i][3],
-            "pinyin": dictionary_data[i][4],
-            "ph-tl": dictionary_data[i][5],
-            }
-        return jsonify(result)
-    except IndexError:
-        message = "At the moment, the dictionary only have {} items.".format(dictionary_len)
-        return render_template("error.html",error=message)
+    return dictionary_api.get_json_one(int(param))
 
+#Error handling:
 @app.errorhandler(404)
 @app.errorhandler(500)
 def page_not_found(e):
